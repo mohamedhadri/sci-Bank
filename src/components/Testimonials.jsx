3cdccf3fa5e131f83888dd754e20a8f441df60c6
Mohamed Hadri <mhadri1000@gmail.com>
2022-09-20 17:36:23 +0300

    fixed the img array

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -5,7 +5,7 @@ export const feedback = [
       "Money is only a tool. It will take you wherever you wish, but it will not replace you as the driver.",
     name: "Herman Jensen",
     title: "Founder & Leader",
-    img: people01,
+    img: '../assets/people01.png',
   },
   {
     id: "feedback-2",
@@ -13,7 +13,7 @@ export const feedback = [
       "Money makes your life easier. If you're lucky to have it, you're lucky.",
     name: "Steve Mark",
     title: "Founder & Leader",
-    img: people02,
+    img: '../assets/people02.png',
   },
   {
     id: "feedback-3",
@@ -21,7 +21,7 @@ export const feedback = [
       "It is usually people in the money business, finance, and international trade that are really rich.",
     name: "Kenn Gallagher",
     title: "Founder & Leader",
-    img: people03,
+    img: '../assets/people03.png',
   },
 ];
 import styles from "../style";
