677a9a9823983cc6324f92a70d18e8cceb73a923
Mohamed Hadri <mhadri1000@gmail.com>
2022-09-20 18:14:02 +0300

    tt

--- src/components/FeedBackCard.jsx
+++ src/components/FeedBackCard.jsx
@@ -1,5 +1,6 @@
 
 import { quotes } from "../assets";
+import React from "react";
 
 const FeedbackCard = ({ content, name, title, img }) => (
   <div className="flex justify-between flex-col px-10 py-12 rounded-[20px]  max-w-[370px] md:mr-10 sm:mr-5 mr-0 my-5 feedback-card">
