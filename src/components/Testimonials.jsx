514ad4037ee81666a8acfefbedc1d3a378f62b79
Mohamed Hadri <mhadri1000@gmail.com>
2022-09-20 17:51:00 +0300

    fixed feedback card err

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -25,6 +25,7 @@ export const feedback = [
   },
 ];
 import styles from "../style";
+import FeedbackCard from "./FeedbackCard";
 
 const Testimonials = () => (
   <section id="clients" className={`${styles.paddingY} ${styles.flexCenter} flex-col relative `}>
