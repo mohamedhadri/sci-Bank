4813ba4f813e7dd29dc37d1f55d7fd0d870e9a00
Mohamed Hadri <mhadri1000@gmail.com>
2022-09-20 16:15:23 +0300

    test

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -13,7 +13,7 @@ const Testimonials = () => (
       <div className="w-full md:mt-0 mt-6">
         <p className={`${styles.paragraph} text-left max-w-[450px]`}>
           Everything you need to accept card payments and grow your business
-          anywhere on the planet.
+          anywhere on the planet..
         </p>
       </div>
     </div>
